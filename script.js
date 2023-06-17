2cfc4960f22fe8f0fbda59e5424423b81e8a0058
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-06-17 11:19:08 -0300

    Atualização script
    Ajuste no script para o funcionamento.
    Ajuste nos botões de ação e no inputs.

--- script.js
+++ script.js
@@ -9,50 +9,70 @@ const adicionarBtn = document.querySelector('#contagemBtn')
 const inputTempo = document.querySelector('.inputTempo')
 const txtHora = document.querySelector('#txtHora')
 const txtMinuto = document.querySelector('#txtMinuto')
+const resetBtn = document.querySelector('#resetBtn')
 
-const t = true
 let interval
 let horas = 0
-let minutos =0
-let segundos = 60
+let minutos = 0
+let segundos = 0
 let millisegundos = 1000
 let pausado = false
 
 adicionarBtn.addEventListener("click", adicionar)
 playBtn.addEventListener("click", contagem)
+pauseBtn.addEventListener("click", pausar)
+resumeBtn.addEventListener("click", retomar)
+
 
 function adicionar() {
-    if (t === true) {
-        inputTempo.style.display = 'block'
-        t = false   
+    if (pausado === !true) {
+        inputTempo.style.display = 'block' 
     }
 }
 
 function contagem() {
     horas = Number(txtHora.value)
     minutos = Number(txtMinuto.value)
-    interval = setInterval(() => {
-        if (!pausado){
-            millisegundos -= 10
+    horasEl.innerHTML = horas
+    minutosEl.innerHTML = minutos
+    if(horas === 0 && minutos === 0 && segundos === 0){
+        alert("Preencha o campo corretamente.")
+    } else {
+        interval = setInterval(() => {
+            if (!pausado){
+                millisegundos -= 10
 
-            if (millisegundos === 0) {
-                segundos--
-                millisegundos = 1000
+                if (millisegundos === 0) {
+                    segundos--
+                    millisegundos = 1000
+                }
+                if (minutos === 0 && segundos === 0){
+                    pausado = true
+                    playBtn.style.display = 'block'
+                    pauseBtn.style.display = 'none'
+                } else if(segundos === 0){
+                    minutos--
+                    segundos = 59
+                }
             }
-            if (segundos === 0){
-                minutos--
-                segundos = 60
-            }
+            horasEl.innerHTML = horas
+            minutosEl.innerHTML = minutos
+            segundosEl.innerHTML = segundos
+        }, 10)
 
-        }
-        horasEl.innerHTML = horas
-        minutosEl.innerHTML = minutos
-        segundosEl.innerHTML = segundos
-    }, 10)
-
-
-
-    inputTempo.style.display = 'none'
-    playBtn.style.display = 'none'
+        inputTempo.style.display = 'none'
+        playBtn.style.display = 'none'
+        adicionarBtn.style.display = 'none'
+        pauseBtn.style.display = 'block'
+        resetBtn.style.display = 'block'
+    }}
+function pausar(){
+    pausado = true
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'block'
+}
+function retomar(){
+    pausado = false
     pauseBtn.style.display = 'block'
+    resumeBtn.style.display = 'none'
 }
