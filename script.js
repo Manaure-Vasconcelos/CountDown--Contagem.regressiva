08d9d96e3ab8c56f0302821777b2c1b090302195
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-06-16 21:39:52 -0300

    atualização script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const horas = document.querySelector('.hora')
-const minutos = document.querySelector('.minutos')
-const segundos = document.querySelector('.segundos')
-const separador = document.querySelector('.separador')
+const horasEl = document.querySelector('#horas')
+const minutosEl = document.querySelector('#minutos')
+const segundosEl = document.querySelector('#segundos')
+const separadorEl = document.querySelector('#separador')
 const playBtn = document.querySelector('#playBtn')
 const pauseBtn = document.querySelector('#pauseBtn')
 const resumeBtn = document.querySelector('#resumeBtn')
@@ -9,7 +9,14 @@ const adicionarBtn = document.querySelector('#contagemBtn')
 const inputTempo = document.querySelector('.inputTempo')
 const txtHora = document.querySelector('#txtHora')
 const txtMinuto = document.querySelector('#txtMinuto')
+
 const t = true
+let interval
+let horas = 0
+let minutos =0
+let segundos = 60
+let millisegundos = 1000
+let pausado = false
 
 adicionarBtn.addEventListener("click", adicionar)
 playBtn.addEventListener("click", contagem)
@@ -22,6 +29,29 @@ function adicionar() {
 }
 
 function contagem() {
+    horas = Number(txtHora.value)
+    minutos = Number(txtMinuto.value)
+    interval = setInterval(() => {
+        if (!pausado){
+            millisegundos -= 10
+
+            if (millisegundos === 0) {
+                segundos--
+                millisegundos = 1000
+            }
+            if (segundos === 0){
+                minutos--
+                segundos = 60
+            }
+
+        }
+        horasEl.innerHTML = horas
+        minutosEl.innerHTML = minutos
+        segundosEl.innerHTML = segundos
+    }, 10)
+
+
+
     inputTempo.style.display = 'none'
     playBtn.style.display = 'none'
     pauseBtn.style.display = 'block'
