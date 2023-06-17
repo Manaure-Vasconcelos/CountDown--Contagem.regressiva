e3b9ba62c50bc3b1c64472b37483596c3147e019
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-06-17 13:28:47 -0300

    Script finalizado

--- script.js
+++ script.js
@@ -22,19 +22,18 @@ adicionarBtn.addEventListener("click", adicionar)
 playBtn.addEventListener("click", contagem)
 pauseBtn.addEventListener("click", pausar)
 resumeBtn.addEventListener("click", retomar)
+resetBtn.addEventListener("click", resetar)
 
 
 function adicionar() {
-    if (pausado === !true) {
+    if (pausado === !true) 
         inputTempo.style.display = 'block' 
-    }
+        txtHora.focus()
 }
 
 function contagem() {
     horas = Number(txtHora.value)
     minutos = Number(txtMinuto.value)
-    horasEl.innerHTML = horas
-    minutosEl.innerHTML = minutos
     if(horas === 0 && minutos === 0 && segundos === 0){
         alert("Preencha o campo corretamente.")
     } else {
@@ -42,7 +41,7 @@ function contagem() {
             if (!pausado){
                 millisegundos -= 10
 
-                if (millisegundos === 0) {
+                if (millisegundos === 0){
                     segundos--
                     millisegundos = 1000
                 }
@@ -55,9 +54,9 @@ function contagem() {
                     segundos = 59
                 }
             }
-            horasEl.innerHTML = horas
-            minutosEl.innerHTML = minutos
-            segundosEl.innerHTML = segundos
+            horasEl.innerHTML = formatTempo(horas)
+            minutosEl.innerHTML = formatTempo(minutos)
+            segundosEl.innerHTML = formatTempo(segundos)
         }, 10)
 
         inputTempo.style.display = 'none'
@@ -75,4 +74,28 @@ function retomar(){
     pausado = false
     pauseBtn.style.display = 'block'
     resumeBtn.style.display = 'none'
+}
+
+function resetar(){
+    clearInterval(interval)
+    horas = 0
+    minutos = 0
+    segundos = 0
+    millisegundos = 1000
+
+    horasEl.innerHTML = "00"
+    minutosEl.innerHTML = "00"
+    segundosEl.innerHTML = "00"
+    txtHora.value = ''
+    txtMinuto.value = ''
+
+    playBtn.style.display = 'block'
+    adicionarBtn.style.display = 'block'
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'none'
+    resetBtn.style.display = 'none'
+}
+
+function formatTempo(T){
+    return T < 10 ? `0${T}` : T
 }
